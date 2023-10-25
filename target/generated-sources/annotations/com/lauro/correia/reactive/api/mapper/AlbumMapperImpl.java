5bd5a961de29db5265104fb4464b606e1c16d565
Lauro Correia Silveira <lauro.silveira@outlook.com.br>
2023-10-26 01:46:10 +0200

    Added Gihub Action Docker build and push to DockerHub (#16)
    - Added Github Action to build and push image to DockerHub

---
+++ target/generated-sources/annotations/com/lauro/correia/reactive/api/mapper/AlbumMapperImpl.java
@@ -0,0 +1,49 @@
+package com.lauro.correia.reactive.api.mapper;
+
+import com.lauro.correia.reactive.api.model.Album;
+import com.lauro.correia.reactive.api.vo.AlbumVO;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-10-25T12:54:25+0200",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.8 (Homebrew)"
+)
+@Component
+public class AlbumMapperImpl implements AlbumMapper {
+
+    @Override
+    public List<AlbumVO> mapToAlbumVO(List<Album> album) {
+        if ( album == null ) {
+            return null;
+        }
+
+        List<AlbumVO> list = new ArrayList<AlbumVO>( album.size() );
+        for ( Album album1 : album ) {
+            list.add( albumToAlbumVO( album1 ) );
+        }
+
+        return list;
+    }
+
+    protected AlbumVO albumToAlbumVO(Album album) {
+        if ( album == null ) {
+            return null;
+        }
+
+        String userId = null;
+        String albumId = null;
+        String title = null;
+
+        userId = album.userId();
+        albumId = album.albumId();
+        title = album.title();
+
+        AlbumVO albumVO = new AlbumVO( userId, albumId, title );
+
+        return albumVO;
+    }
+}
---
+++ target/generated-sources/annotations/com/lauro/correia/reactive/api/mapper/PostMapperImpl.java
@@ -0,0 +1,51 @@
+package com.lauro.correia.reactive.api.mapper;
+
+import com.lauro.correia.reactive.api.model.Post;
+import com.lauro.correia.reactive.api.vo.PostVO;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-10-25T12:54:25+0200",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.8 (Homebrew)"
+)
+@Component
+public class PostMapperImpl implements PostMapper {
+
+    @Override
+    public List<PostVO> maptToVo(List<Post> post) {
+        if ( post == null ) {
+            return null;
+        }
+
+        List<PostVO> list = new ArrayList<PostVO>( post.size() );
+        for ( Post post1 : post ) {
+            list.add( postToPostVO( post1 ) );
+        }
+
+        return list;
+    }
+
+    protected PostVO postToPostVO(Post post) {
+        if ( post == null ) {
+            return null;
+        }
+
+        String userId = null;
+        String postId = null;
+        String title = null;
+        String body = null;
+
+        userId = post.userId();
+        postId = post.postId();
+        title = post.title();
+        body = post.body();
+
+        PostVO postVO = new PostVO( userId, postId, title, body );
+
+        return postVO;
+    }
+}
