12e5bb9411fc534383a764c9450c28f6200eec36
Lauro Correia Silveira <lauro.silveira@outlook.com.br>
2023-10-25 23:00:31 +0200

    Added dockerignore

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM eclipse-temurin:21-alpine
 VOLUME /tmp
-COPY target/reactive-api-0.0.1-SNAPSHOT.jar reactive-api.jar
+COPY target/*.jar  reactive-api.jar
 ENTRYPOINT ["java", "-jar", "/reactive-api.jar"]
---
+++ target/generated-sources/annotations/com/lauro/correia/reactive/api/mapper/CompanyMapperImpl.java
@@ -0,0 +1,34 @@
+package com.lauro.correia.reactive.api.mapper;
+
+import com.lauro.correia.reactive.api.model.Company;
+import com.lauro.correia.reactive.api.vo.CompanyVO;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-10-25T12:54:25+0200",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.8 (Homebrew)"
+)
+@Component
+public class CompanyMapperImpl implements CompanyMapper {
+
+    @Override
+    public CompanyVO maptoCompanyVO(Company company) {
+        if ( company == null ) {
+            return null;
+        }
+
+        String name = null;
+        String catchPhrase = null;
+        String bs = null;
+
+        name = company.name();
+        catchPhrase = company.catchPhrase();
+        bs = company.bs();
+
+        CompanyVO companyVO = new CompanyVO( name, catchPhrase, bs );
+
+        return companyVO;
+    }
+}
