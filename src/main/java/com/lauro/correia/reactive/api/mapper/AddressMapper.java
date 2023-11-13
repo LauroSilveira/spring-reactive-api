cee6b906248598c666adcc07acd4c83c9137269a
Lauro Correia Silveira <lauro.silveira@outlook.com.br>
2023-11-13 08:20:34 +0100

    Renamed GitHub actions and add inyection Strategy for mappers (#27)

--- src/main/java/com/lauro/correia/reactive/api/mapper/AddressMapper.java
+++ src/main/java/com/lauro/correia/reactive/api/mapper/AddressMapper.java
@@ -2,10 +2,11 @@ package com.lauro.correia.reactive.api.mapper;
 
 import com.lauro.correia.reactive.api.model.Address;
 import com.lauro.correia.reactive.api.vo.AddressVO;
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring", uses = {GeolocationMapper.class})
+@Mapper(componentModel = "spring", uses = {GeolocationMapper.class}, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface AddressMapper {
 
     @Mapping(source = "geo", target = "geolocation")
