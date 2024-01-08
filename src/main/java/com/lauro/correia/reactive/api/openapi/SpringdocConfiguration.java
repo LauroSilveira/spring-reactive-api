c31abe2cc3467a77bfbfa9c4642d0fa3941db24c
Lauro Correia Silveira <lauro.silveira@outlook.com.br>
2024-01-08 16:17:46 +0100

    Added proxy server

--- src/main/java/com/lauro/correia/reactive/api/openapi/SpringdocConfiguration.java
+++ src/main/java/com/lauro/correia/reactive/api/openapi/SpringdocConfiguration.java
@@ -4,14 +4,18 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SpringdocConfiguration {
   @Bean
   public OpenAPI customOpenAPI() {
     return new OpenAPI()
+            .servers(List.of(new Server().url("/").description("Default")))
         .info(new Info().title("spring-reactive-api")
             .description("An reactive API using Spring WebFlux")
             .contact(new Contact()
